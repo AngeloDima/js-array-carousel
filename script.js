15bb1e2e0f843a63237b66c1c30e5efc9d79f1d1
Angelo <Angelodimauro91@gmail.com>
2022-08-29 18:18:14 +0200

    js (pt1)

---
+++ script.js
@@ -0,0 +1,5 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-freccia-prev");
+const nextButton = document.getElementById("slide-freccia-next");
+
